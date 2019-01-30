11444e1209284a0392d6ac3a3926e15c0e0de400
mi kyung suh <kangs@miui-MacBook-Pro.local>
2019-01-29 18:52:07 -0600

    Olivia assessment 1

--- src/assessment.py
+++ src/assessment.py
@@ -4,8 +4,8 @@ Write your code under the commented(#) question. Replace the pass with your code
 
 ### Question1
 ### Print 2**0 + 2**1 + 2**2
+print( 2**0 + 2**1 + 2**2)
 
-pass
 
 
 
@@ -14,4 +14,4 @@ pass
 
 A = [17, 14, 14, 14, 16, 15, 16, 14, 15, 13, 18, 13]
 
-pass
+sum(A)
